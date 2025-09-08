3e7d644bec3172ede6220cac00dcd4eccf2a1dcc
Aditya Kadlimatti <220306392+Aditya-Dishoom@users.noreply.github.com>
2025-09-08 17:32:11 +0530

    testing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "haan chal rha hai bhai"
+   return "Hello CICD Pipeline - Automated Deployment Successful"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
--- test.py
+++ test.py
@@ -12,4 +12,4 @@ def client():
 def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"haan chal rha hai bhai" in response.data
+    assert b"Hello CICD Pipeline - Automated Deployment Successful" in response.data
